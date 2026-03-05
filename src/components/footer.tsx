29c64829d8e36540b43bc301a515169ed0f84a20
Achyut Paliwal <achyutkpaliwal@gmail.com>
2026-03-05 23:52:28 +0300

    20th commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer(){
     return(
-        <div className="flex flex-row p-4 justify-between items-center text-md">
+        <div className="flex flex-row p-4 justify-around items-center text-md">
             <div className="">@2026. All rights reserved</div>
             <a href="https://github.com/Achyut2009">Made by Achyut Paliwal</a>
         </div>
